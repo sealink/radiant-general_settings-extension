21088ae6ae26aeb86d9a7d605e1bfe00b2575b15
Michael Noack <michael@noack.com.au>
2013-05-31 13:41:43 +0930

    Make general settings work with symbol keys

--- app/models/general_setting.rb
+++ app/models/general_setting.rb
@@ -3,7 +3,7 @@ class GeneralSetting < ActiveRecord::Base
   validates_uniqueness_of :key, :message => "Key has already been used."
   
   def self.[](key)
-    self.find_by_key(key).try(:value)
+    self.find_by_key(key.to_s).try(:value)
   end
 
 end
